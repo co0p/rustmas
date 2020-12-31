5d458d30cc047307458da6c5a4f9c7f2dc8707d4
Julian Godesa <julian.godesa@googlemail.com>
2020-12-31 17:14:28 +0100

    looping works

--- src/day01.rs
+++ src/day01.rs
@@ -1,11 +1,28 @@
-use std::fs::File as fs;
+use std::io::{self, BufRead};
+use std::fs::File;
+use std::path::Path;
+use std::vec::Vec;
 
 pub fn solve() {
  
+    let mut inputs = Vec::new();
+
     // read file line by line
-    let input = fs::open("input_day01.txt");
-    
-    for line in input.iter() {
-        println!("{}", line);
+    if let Ok(lines) = read_lines("input_day01.txt") {
+
+        for line in lines {
+            if let Ok(entry) = line {
+                let number : u32 = entry.parse().unwrap();
+                inputs.push(number)
+            }
+        }
     }
+
+    println!("we have {} entries", inputs.len())
+}
+
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
