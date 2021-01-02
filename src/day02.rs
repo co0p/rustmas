963049f52dad11bd8c0418c680a3e404a07a8e02
Julian Godesa <julian.godesa@googlemail.com>
2021-01-02 16:46:45 +0100

    wip day02

--- src/day02.rs
+++ src/day02.rs
@@ -1,10 +1,36 @@
+use std::io;
+use std::io::BufRead;
+use std::fs::File;
+use std::path::Path;
 use std::string::String;
 
 pub fn solve() {
-	// read input into string vector
+        // read file line by line
+        let mut counter = 1;
+        if let Ok(lines) = read_lines("input_day02.txt") {
+            for line in lines {
+                
+                if let Ok(entry) = line {
+                    let entry2 = entry;
+                    println!("{}: {}", counter, valid(entry2));
+                    counter = counter + 1;
+                }
+            }
+        }
+
 	println!("calling solve {}", valid("1-2 a: abcde".to_string()));
 }
 
 fn valid(candidate: String) -> bool {
-	true
+    print!("{}", candidate);
+	return true
+}
+
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
