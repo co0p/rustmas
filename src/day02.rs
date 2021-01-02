293edaca074dd4f8bf5d17e3225193678f73cef2
jev <eb120@hw.ac.uk>
2021-01-02 15:31:39 +0000

    sketch day2

---
+++ src/day02.rs
@@ -0,0 +1,10 @@
+use std::string::String;
+
+pub fn solve() {
+	// read input into string vector
+	println!("calling solve {}", valid("1-2 a: abcde".to_string()));
+}
+
+fn valid(candidate: String) -> bool {
+	true
+}
